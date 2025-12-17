97663ea85a88620fa780a30581bd16052844d0ce
smathj <smathj@naver.com>
2025-12-18 08:05:53 +0900

    step9: DB 연동 메세지큐의 트랜잭션 처리 개요

---
+++ src/main/java/net/harunote/hellomessagequeue/step9/TransactionController.java
@@ -0,0 +1,32 @@
+package net.harunote.hellomessagequeue.step9;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/message")
+@RequiredArgsConstructor
+public class TransactionController {
+
+
+    private final MessageProducer messageProducer;
+
+
+    @PostMapping()
+    public ResponseEntity<String> sendMessage(@RequestBody StockEntity stockEntity,
+                                              @RequestParam(required = false, defaultValue = "success") String testCase) {
+        // do something
+        System.out.println("Send message : " + stockEntity);
+
+        try {
+            messageProducer.sendMessage(stockEntity, testCase);
+            return ResponseEntity.ok("Message sent successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("MQ 트랜잭션 실패 : " + e.getMessage());
+        }
+
+    }
+}
